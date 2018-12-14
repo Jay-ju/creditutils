956d527e2afb8f81565fd5248864c0a70e022bcf
caifh <caifh@app.com>
2018-12-14 17:32:18 +0800

    1.增加测试目录；

--- creditutils/tests/test_apk_util.py
+++ creditutils/tests/test_apk_util.py
@@ -39,7 +39,8 @@ def test_align_apk():
 
 def test_align_check_apk():
     # src_file = r'E:\temp\apk\3.5.4\3.5.4beta_p_01-487-20181206_sec_1.apk'
-    src_file = r'E:\temp\apk\3.5.4\3.5.4beta_p_01-487-20181206_sec_2.apk'
+    # src_file = r'E:\temp\apk\3.5.4\3.5.4beta_p_01-487-20181206_sec_2.apk'
+    src_file = r'E:\temp\apk\3.5.4\3.5.4beta_p_01-487-20181206_sec_1_yingyb.apk'
     result = apk_util.zipalign_check(src_file)
     info = "The file's align status: {}.".format(result)
     print(info)
